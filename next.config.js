437c7123d9ff9132a867d9cac824f6ff69752e8b
rajesh tamire <rajesh.tamire@gmail.com>
2024-03-18 18:08:01 +0000

    create invoice completed

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        serverActions: {
+            allowedOrigins: ['x3q6cp-3000.csb.app']
+        }
+    }
+};
 
 module.exports = nextConfig;
